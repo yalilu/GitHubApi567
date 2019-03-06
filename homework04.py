fd8d71dc46c40ba3f5b1c2349bec6843bcbebc42
yalilu <47368428+yalilu@users.noreply.github.com>
2019-03-05 23:10:41 -0500

    Update homework04.py

--- homework04.py
+++ homework04.py
@@ -8,18 +8,21 @@ Created on Tue Feb 19 10:10:37 2019
 
 import requests
 import json
-'''response_list=[]'''
-repo_dict={}
-url = 'https://api.github.com/users/yalilu/repos'
 
-r=requests.get(url)
-print(r.status_code)
+class get_api():
+    repo_dict={}
+    def get_info_api(name):
+        url = 'https://api.github.com/users/'+name+'/repos'
+        r=requests.get(url)
+        print(r.status_code)
 
-print("The user has "+str(len(r.json())) + " repos, names and commits as follows: ")
-for item in r.json():
-    url_comm = 'https://api.github.com/repos/yalilu/'+item['name']+'/commits'
-    s=requests.get(url_comm)
-    commits_list=s.json()
-    commits_dict=commits_list[0]
-    print("Name: " +item['name']+"; Commits: "+str(len(commits_dict)))
-    
+
+        print("The user has "+str(len(r.json())) + " repos, names and commits as follows: ")
+        for item in r.json():
+            url_comm = 'https://api.github.com/repos/'+name+'/'+item['name']+'/commits'
+            s=requests.get(url_comm)
+            commits_list=s.json()
+            commits_dict=commits_list[0]
+            print("Name: " +item['name']+"; Commits: "+str(len(commits_dict)))
+
+        return "Right"
